4b1ae695651cc658275218139f3be4efe802b898
jpmikkers <jean.paul.mikkers@gmail.com>
2022-11-23 14:43:19 +0100

    Tweak about win margins and add version to title

--- PeperNote/AboutWindow.xaml.cs
+++ PeperNote/AboutWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace PeperNote
         {
             InitializeComponent();
 
+            Title=$"About PeperNote v{Assembly.GetEntryAssembly().GetName().Version}";
+
             // https://learn.microsoft.com/en-us/dotnet/desktop/wpf/app-development/wpf-application-resource-content-and-data-files
             var info = Application.GetResourceStream(new Uri("/Resources/About.rtf", UriKind.Relative));
             RichTextBoxAbout.Selection.Load(info.Stream, DataFormats.Rtf);
