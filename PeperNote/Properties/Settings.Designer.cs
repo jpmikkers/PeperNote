dba4ad6f11fbcc742a660f03d8190c24be5479f8
jpmikkers <jean.paul.mikkers@gmail.com>
2025-01-15 14:10:26 +0100

    dotnet 9

--- PeperNote/Properties/Settings.Designer.cs
+++ PeperNote/Properties/Settings.Designer.cs
@@ -12,8 +12,8 @@ namespace PeperNote.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
-    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
